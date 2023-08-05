e2404c0b82e884626e0a6d792b295de9098ff028
Sudhanshu Sonare <sudhanshusonare@Sudhanshus-MacBook-Air.local>
2023-08-05 10:09:12 +0530

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const parent = React.createElement(
   "div",
   {id : "heading"},
   [
     React.createElement("div",{id: "child", xyz: "abc"}, [
-      React.createElement("h1", {}, "Hello This is my first text in React"),
+      React.createElement("h1", {}, "Welcome to Namaste react 🚀"),
       React.createElement("h2", {}, "Hello This is my second text in React"),
     ]),
     React.createElement("div",{id: "child2", xyz: "abc2"}, [
