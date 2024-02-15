175d2d603428f49e7a45ae3b5bc7c4e8a695d6f2
Sudhanshu Sonare <76194635+Sudhanshu3010@users.noreply.github.com>
2024-02-16 01:43:58 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
       Created By
       <i className="fa-solid fa-heart"></i>
       <a href="https://www.linkedin.com/in/tejas-mete-782b6521a/" target="_blank">
-        Tejas Mete
+        Sudhanshu Sonare
       </a>
       <i className="fa-solid fa-copyright"></i>
       {year}
